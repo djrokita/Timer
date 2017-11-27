9fd40591822107abdd9c292b7043623b01f56429
Krzych <krzych.pierzchala@gmail.com>
2017-11-27 16:06:14 +0100

    Adjusted code js to ES6 (const instead of var)

--- script.js
+++ script.js
@@ -1,81 +1,82 @@
 class Stopwatch {
-	constructor(display) {
-		this.running = false;
-		this.display = display;
-		this.reset();
-		this.print(this.times);
-	}
-	reset() {
-		if (!this.running) {
-			this.times = {
-				minutes: 0,
-				seconds: 0,
-				miliseconds: 0
-			}
-		};
-		this.print();
-	}
-	print() {
-		this.display.innerText = this.format(this.times);
-	}
-	format(times) {
-		return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
-	}
-	start() {
-		if (!this.running) {
-			this.running = true;
-			this.watch = setInterval(() => this.step(), 10);
-		}
-	}
-	stop () {
-		if (this.running) {
-			this.running = false;
-			clearInterval(this.watch);			
-		}
-		else {
-			this.reset();
-		}
-	}
-	step() {
-		if (!this.running) return;
-		this.calculate();
-		this.print();
-	}
-	calculate() {
-		this.times.miliseconds += 1;
-		if (this.times.miliseconds >= 100) {
-			this.times.seconds += 1;
-			this.times.miliseconds = 0;
-		}
-		if (this.times.seconds >= 60) {
-			this.times.minutes += 1;
-			this.times.seconds = 0;
-		}
-	}
-	shot() {
-		let timeshot = this.format(this.times);
-		let item = document.createElement('li');
-		item.innerHTML = timeshot;
-		resultsList.appendChild(item);
-	}
-	clear() {
-		resultsList.innerHTML = '';
-	}
+  constructor(display) {
+    this.running = false;
+    this.display = display;
+    this.reset();
+    this.print(this.times);
+  }
+  reset() {
+    if (!this.running) {
+      this.times = {
+        minutes: 0,
+        seconds: 0,
+        miliseconds: 0
+      };
+    }
+    this.print();
+  }
+  print() {
+    this.display.innerText = this.format(this.times);
+  }
+  format(times) {
+    return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(
+      Math.floor(times.miliseconds)
+    )}`;
+  }
+  start() {
+    if (!this.running) {
+      this.running = true;
+      this.watch = setInterval(() => this.step(), 10);
+    }
+  }
+  stop() {
+    if (this.running) {
+      this.running = false;
+      clearInterval(this.watch);
+    } else {
+      this.reset();
+    }
+  }
+  step() {
+    if (!this.running) return;
+    this.calculate();
+    this.print();
+  }
+  calculate() {
+    this.times.miliseconds += 1;
+    if (this.times.miliseconds >= 100) {
+      this.times.seconds += 1;
+      this.times.miliseconds = 0;
+    }
+    if (this.times.seconds >= 60) {
+      this.times.minutes += 1;
+      this.times.seconds = 0;
+    }
+  }
+  shot() {
+    let timeshot = this.format(this.times);
+    let item = document.createElement("li");
+    item.innerHTML = timeshot;
+    resultsList.appendChild(item);
+  }
+  clear() {
+    resultsList.innerHTML = "";
+  }
 }
 
 function pad0(value) {
-	let result = value.toString();
-	if (result.length < 2) result = '0' + result;
-	return result;
+  let result = value.toString();
+  if (result.length < 2) result = "0" + result;
+  return result;
 }
 
-const stopwatch = new Stopwatch(document.querySelector('.stopwatch'));
+const stopwatch = new Stopwatch(document.querySelector(".stopwatch"));
 
-var startButton = document.getElementById('start');
-var stopButton = document.getElementById('stop');
-var resultsList = document.querySelector('.results');
+const startButton = document.getElementById("start"); //Zmieniłem z var na const
+const stopButton = document.getElementById("stop");
+const resultsList = document.querySelector(".results");
 
-startButton.addEventListener('click', () => stopwatch.start());
-stopButton.addEventListener('click', () => stopwatch.stop()); //dlaczego nie samo: stopwatch.stop?
-stopwatch.display.addEventListener('click', () => stopwatch.shot());
-resultsList.addEventListener('click', stopwatch.clear);
+startButton.addEventListener("click", () => stopwatch.start());
+stopButton.addEventListener("click", () => stopwatch.stop()); //dlaczego nie samo: stopwatch.stop?
+stopwatch.display.addEventListener("click", () => stopwatch.shot());
+resultsList.addEventListener("click", stopwatch.clear);
